44efac3620f8d92557f64886c5f08924802147bd
AndreMooore <143398486+AndreMooore@users.noreply.github.com>
2024-03-05 12:19:29 +0100

    Komponenter.
    Satt inn komponenter

--- G18/src/App.jsx
+++ G18/src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <section id="innhold">
+      <section id="innhold">
         <nav>
-            <ul id="knapper"></ul>
+          <ul id="knapper"></ul>
         </nav>
-        <main id="aktivtInnhold">
-            
-        </main>
-    </section>
+        <main id="aktivtInnhold"></main>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
