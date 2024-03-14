33896e49cf3a944c9a41a698a3980cc20ff0ab58
MiriamKy <miri.kristiansen@gmail.com>
2024-03-14 10:00:39 +0100

    Lagt inn riktig array for ressursene
    - Ressurser.js (riktig array lagt inn)
    - Content.jsx (kommentar for meg selv)
    - package-lock.json

---
+++ G18/src/components/Content.jsx
@@ -0,0 +1,11 @@
+export default function Content(title, url, category){
+    
+    return 
+    (
+    <>
+    /* Her tenker jeg å skrive ut det som skal stå i innholdsområdet. 
+    /* Det blir en liste med url-er som er filtrert på kategorien som har blitt klikket på.
+    /* Derfor har jeg sendt med noen parametere/props, som da må få verdier, sannsynligvis i et annet component før det brukes til utskrift.
+    /* Jeg tror dette blir metoden å filtrere på
+    </>
+    )
